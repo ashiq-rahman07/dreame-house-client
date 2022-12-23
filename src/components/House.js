4fdbed933da6ae1367ddb4621d3a1789ec77a236
ashiq77 <web3.0.ashiq@gmail.com>
2022-12-24 00:19:19 +0600

    categories serch section add

--- src/components/House.js
+++ src/components/House.js
@@ -3,12 +3,22 @@ import {BiBed, BiBath, BiArea} from 'react-icons/bi';
 const House = ({ house }) => {
   const { image, type, country, address, bedrooms, bathrooms, surfac, price } = house
   return (
-    <div>
-      <img src={image} alt="" />
-      <div>
-        <div>{type}</div>
-        <div>{country}</div>
+    <div className='bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition'>
+      <img className='mb-8' src={image} alt="" />
+      <div className='flex gap-x-2 mb-4 text-sm'>
+        <div className='bg-green-500 text-white rounded-full px-3'>{type}</div>
+        <div className='bg-violet-500 text-white rounded-full px-3'>{country}</div>
       </div>
+      <div className='text-lg font-semibold max-w-[260px]'>
+        {address}
+      </div>
+      <div>
+        <div className='flex gap-1 text-gray-600 items-center'>
+          <div><BiBed /></div>
+          <div>{bedrooms}</div>
+        </div>
+      </div>
+      <div className='text-lg font-semibold text-violet-600 mb-4'>{price}</div>
     </div>
   );
 };
