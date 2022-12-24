aac7438677aa5f2eafadd00bb4f633209dff61a3
ashiq77 <web3.0.ashiq@gmail.com>
2022-12-25 00:42:16 +0600

    single property

--- src/pages/PropertyDetails.js
+++ src/pages/PropertyDetails.js
@@ -6,7 +6,7 @@ import { housesData } from '../data';
 import { BiBed, BiBath, BiArea } from 'react-icons/bi';
 
 //impoert use params
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 const PropertyDetails = () => {
@@ -33,7 +33,7 @@ const PropertyDetails = () => {
           <div className='text-3xl font-semibold text-violet-600'>$ {house.price}</div>
         </div>
         <div className='flex flex-col items-start gap-8 lg:flex-row'>
-          <div className='mx-w-[768px]'>
+          <div className='max-w-[768px]'>
             <div className='mb-8'>
               <img src={house.imageLg} alt="" />
             </div>
@@ -53,15 +53,27 @@ const PropertyDetails = () => {
             </div>
             <div>{house.description}</div>
           </div>
-          <div>
-            <div>
-              <div>
+          <div className='flex-1 bg-white w-full mb-8 border border-gray-300 rounded-lg px-6 py-8'>
+            <div className='flex gap-x-4 mb-8 items-center'>
+              <div className='w-20 h-20 p-1 border border-gray-300 rounded-full'>
                 <img src={house.agent.image} alt="" />
               </div>
               <div>
-                <div>{house.agent.name}</div>
+                <div className='text-lg font-bold'>{house.agent.name}</div>
+                <Link to='' className='text-violet-700 text-sm'>View Listing</Link>
               </div>
             </div>
+            {/* form */}
+            <form className='flex flex-col gap-y-4' action="">
+              <input className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm' type="text" placeholder='Name*' />
+              <input className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm' type="text" placeholder='Email*' />
+              <input className='border border-gray-300 focus:border-violet-700 outline-none rounded w-full px-4 h-14 text-sm' type="text" placeholder='Phone*' />
+              <textarea className='border border-gray-300 focus:border-violet-700 outline-none resize-none rounded w-full p-4 h-36 text-sm text-gray-400' defaultValue='Hello I am interested in'></textarea>
+              <div className='flex gap-x-2'>
+                <button className='bg-violet-700 hover:bg-violet-800 text-white rounded p-4 text-sm w-full transition'>Send message</button>
+                <button className='border border-violet-700 text-violet-700 hover:border-violet-500 hover:text-violet-500 rounded p-4 text-sm w-full transition'>call</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
